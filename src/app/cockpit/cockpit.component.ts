4e80117aad08c67b2d8c68b95fd9a77fde456994
Henrietha <areathy33@gmail.com>
2021-08-16 14:39:31 +0100

    Binding to custom events

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  serverCreated = new EventEmitter<>
+  // blueprintCreated = new
   newServerName = '';
   newServerContent = '';
   
